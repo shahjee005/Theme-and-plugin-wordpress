3475c77b07e13dae5e66e73156c3bd08523ced53
Syed-Gardezi <61136718+shahjee005@users.noreply.github.com>
2020-05-04 14:47:37 -0600

    added date

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
-
+<footer>
+    <h2>Footer</h2>
+    <p>
+      &copy;
+      <?php echo date( 'Y' ); // Use PHP to keep the year current! Less work for us! ?>
+      TECHCareers by Manpower
+    </p>
+  </footer>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,17 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
     <?php
-    //Output our site title
-    bloginfo('name');
-    //show page info if its avilable 
-    if ($current_post_title= get_the_title()) {
-        echo '|'. $current_post_title;}
-        ?>
-    </title>
+      // Output our site title.
+      bloginfo( 'name' );
+      // Show page info (if available.)
+      if ( $current_post_title = get_the_title() ) {
+        echo ' | ' . $current_post_title;
+      }
+    ?>
+  </title>
 </head>
 <body>
-    
+  <h1>
+    <?php
+      // Output our site title.
+      bloginfo( 'name' );
+      // Show page info (if available.)
+      if ( $current_post_title = get_the_title() ) {
+        echo ' | ' . $current_post_title;
+      }
+    ?>
+  </h1>
+
